84aced84be33a64aee2e99875e54f20521309ce7
alkouthershop-debug <alkouthershop@gmail.com>
2026-01-27 19:31:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+ document.getElementById('payment-method').addEventListener('change', function() {
+       
+        // إظهار الخيارات الإضافية عند الاختيار
+        if (this.value === 'cash' || this.value === 'card') {
+            // يمكنك إضافة أي تعديلات هنا
+        }
+    });
